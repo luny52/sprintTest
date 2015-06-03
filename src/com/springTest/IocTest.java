d9c157340dc69f43a5b79e1e641821be1afed889
luny52 <luny52@163.com>
2015-06-03 14:30:03 +0800

    aop

--- src/com/springTest/IocTest.java
+++ src/com/springTest/IocTest.java
@@ -4,6 +4,9 @@ import org.apache.catalina.core.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.springTest.aop.AopTestBean;
+import com.springTest.aop.AopTestInterface;
+
 public class IocTest {
 
 	public static void main(String[] args) {
@@ -12,6 +15,8 @@ public class IocTest {
 		Testspring hello=(Testspring) contest.getBean("testspring");
 	        System.out.println(hello.Excute());
 
+	        AopTestInterface aoptest=(AopTestInterface) contest.getBean("aopTestBean");
+	        aoptest.save("TestAop");
 	}
 
 }
