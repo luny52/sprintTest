3f1be9f1b6002c0d55667f19f404df8a72931bc3
luny52 <luny52@163.com>
2015-08-06 13:39:33 +0800

    增加同步测试

--- src/com/springTest/controllers/Page1Controller.java
+++ src/com/springTest/controllers/Page1Controller.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.springTest.Testspring;
 
 //引入ModelAndView时要注意spring有两个ModelAndView,
 //import org.springframework.web.portlet.ModelAndView这个不是我们要的ModelAndView引入它不会出错,
@@ -22,17 +27,25 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Page1Controller {
 	private PrintWriter writer;
-	
+
 	@RequestMapping("/page1-1")
-	public @ResponseBody() String ShowMsg() {
-		return "This is Page1Controller";
+	public @ResponseBody() String ShowMsg(HttpServletRequest request) {
+
+		ServletContext servletContext = request.getSession()
+				.getServletContext();
+		ApplicationContext context = WebApplicationContextUtils
+				.getWebApplicationContext(servletContext);
+
+		Testspring hello = (Testspring) context.getBean("testspring");
+
+		return "This is Page1Controller" + hello.Excute();
 	}
 
 	@RequestMapping("/page1-3")
 	public @ResponseBody() void ShowVoid(@RequestParam("uid") String uid,
 			HttpServletRequest request) {
-		//注意ShowVoid方法加上参数uid后请求的URL中必须包含参数uid才会被拦截到
-		//HttpServletRequest request 可有可无
+		// 注意ShowVoid方法加上参数uid后请求的URL中必须包含参数uid才会被拦截到
+		// HttpServletRequest request 可有可无
 		String RequestURI = request.getRequestURI();
 		String QueryString = request.getQueryString();
 		String RequestURL = request.getRequestURL().toString();
@@ -47,9 +60,10 @@ public class Page1Controller {
 
 	@RequestMapping("/topage1")
 	public String StringToPage1() {
+
 		return "page1";
 	}
-	
+
 	@RequestMapping("/cometpage")
 	public String StringTocometpage() {
 		return "../cometpage";
@@ -71,12 +85,12 @@ public class Page1Controller {
 
 		return mv;
 	}
-	
+
 	@RequestMapping("/comet")
 	public @ResponseBody() void Showcomet(@RequestParam("uid") String uid,
-			HttpServletRequest request,HttpServletResponse response) {
-		//注意ShowVoid方法加上参数uid后请求的URL中必须包含参数uid才会被拦截到
-		//HttpServletRequest request 可有可无
+			HttpServletRequest request, HttpServletResponse response) {
+		// 注意ShowVoid方法加上参数uid后请求的URL中必须包含参数uid才会被拦截到
+		// HttpServletRequest request 可有可无
 		String RequestURI = request.getRequestURI();
 		String QueryString = request.getQueryString();
 		String RequestURL = request.getRequestURL().toString();
@@ -86,17 +100,17 @@ public class Page1Controller {
 		String Method = request.getMethod();
 		String Parameter = request.getParameter("uid");
 
-		 try {
+		try {
 			writer = response.getWriter();
 		} catch (IOException e1) {
 			// TODO 自动生成的 catch 块
 			e1.printStackTrace();
 		}
-		
-		while(true)
-		{
+
+		while (true) {
 			try {
-				writer.print("<script type=\"text/javascript\">window.parent.Change('" + new Date().toString() + "')</script>");
+				writer.print("<script type=\"text/javascript\">window.parent.Change('"
+						+ new Date().toString() + "')</script>");
 				writer.flush();
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -104,9 +118,8 @@ public class Page1Controller {
 				e.printStackTrace();
 			}
 		}
-		
-//		return ;
+
+		// return ;
 	}
-	
 
 }
